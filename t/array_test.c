e625407dbf9bd2824fe3a1d269cbf9e851f734d2
Matthew Zito <exbotanical@protonmail.com>
2022-12-26 21:25:34 -0600

    update clib src

--- t/array_test.c
+++ t/array_test.c
@@ -1,52 +1,48 @@
-#include "tap.c/tap.h"
-
 #include "array.h"
 
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
+
+#include "tap.c/tap.h"
 
 void test_ch_array_init() {
-	ch_array_t *ca;
+  ch_array_t *ca;
 
-	lives_ok({
-		ca = ch_array_init();
-	}, "initializes the char array");
+  lives_ok({ ca = ch_array_init(); }, "initializes the char array");
 
-	ok(ca->size == 0, "newly initialized array's size is 0");
-	ok(strcmp(ca->state, "") == 0, "newly initialized array's state is empty");
+  ok(ca->size == 0, "newly initialized array's size is 0");
+  ok(strcmp(ca->state, "") == 0, "newly initialized array's state is empty");
 
-	ch_array_free(ca);
+  ch_array_free(ca);
 }
 
 void test_ch_array_insert() {
-	ch_array_t *ca = ch_array_init();
+  ch_array_t *ca = ch_array_init();
 
-	ch_array_insert(ca, "test");
+  ch_array_insert(ca, "test");
 
-	is(ca->state[0], "test", "array contains value at index position");
-	is(ca->state[1], NULL, "array contains NULL at index position");
+  is(ca->state[0], "test", "array contains value at index position");
+  is(ca->state[1], NULL, "array contains NULL at index position");
 
-	ch_array_insert(ca, "str");
-	is(ca->state[0], "test", "array contains value at index position");
-	is(ca->state[1], "str", "array contains value at index position");
+  ch_array_insert(ca, "str");
+  is(ca->state[0], "test", "array contains value at index position");
+  is(ca->state[1], "str", "array contains value at index position");
 
-	ch_array_free(ca);
+  ch_array_free(ca);
 }
 
 void test_ch_array_free() {
-	ch_array_t *ca = ch_array_init();
+  ch_array_t *ca = ch_array_init();
 
-	lives_ok({
-		ch_array_free(ca);
-	}, "frees the allocated char array");
+  lives_ok({ ch_array_free(ca); }, "frees the allocated char array");
 }
 
-int main (int argc, char *argv[]) {
-	plan(8);
+int main(int argc, char *argv[]) {
+  plan(8);
 
-	test_ch_array_init();
-	test_ch_array_insert();
-	test_ch_array_free();
+  test_ch_array_init();
+  test_ch_array_insert();
+  test_ch_array_free();
 
   done_testing();
 }
