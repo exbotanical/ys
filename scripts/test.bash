68dfc083b357df1dde32bdccc99697bb147fcd2f
Matthew Zito <exbotanical@protonmail.com>
2023-03-20 19:55:52 -0500

    chore: make tests less noisy

--- scripts/test.bash
+++ scripts/test.bash
@@ -33,7 +33,7 @@ not_test_file () {
 run_test () {
 	local file_name="$1"
 
-	gcc -D debug -Ideps -Isrc -c "$TESTING_DIR/$file_name" -o main.o
+	gcc -w -D debug -Ideps -Isrc -c "$TESTING_DIR/$file_name" -o main.o
 	gcc -D debug -o main main.o -L./ -l$REPO_DIR -lm -lpcre
 
 	export LD_LIBRARY_PATH=$(pwd)/src/:$(pwd)/include/:$LD_LIBRARY_PATH
