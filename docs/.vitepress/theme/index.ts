bfb697c03e03b19e5c863494c9f87be19337c62a
Matthew Zito <exbotanical@protonmail.com>
2023-04-11 23:59:37 -0500

    chore: update docs theme

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,3 +2,38 @@ import DefaultTheme from 'vitepress/theme'
 import './index.css'
 
 export default DefaultTheme
+
+initNoiseEffect().then(png => {
+  if (!png) return
+  console.log('a')
+  const url = URL.createObjectURL(png)
+  const div = document.createElement('div')
+
+  div.classList.add('noise')
+  ;(div as unknown as { style: string }).style = `background-image: url(${url})`
+
+  document.body.appendChild(div)
+})
+
+function initNoiseEffect() {
+  const size = 64
+  const black = new Uint8ClampedArray([0, 0, 0, 255])
+  const white = new Uint8ClampedArray([255, 255, 255, 255])
+  const data = new Uint8ClampedArray(size * size * 4)
+
+  for (let y = 0; y < size; y++) {
+    for (let x = 0; x < size; x++) {
+      data.set(Math.random() > 0.5 ? white : black, (y * size + x) * 4)
+    }
+  }
+
+  const canvas = document.createElement('canvas')
+  canvas.width = canvas.height = size
+
+  const ctx = canvas.getContext('2d')
+  ctx?.putImageData(new ImageData(data, size, size), 0, 0)
+
+  return new Promise<Blob | null>((resolve, reject) => {
+    canvas.toBlob(b => (b ? resolve(b) : reject()), 'image/png')
+  })
+}
